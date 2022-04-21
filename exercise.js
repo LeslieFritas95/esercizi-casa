9bb5dde5d2042b1150f3128ed205e7513f7aa26b
Leslie <103486449+LeslieFritas95@users.noreply.github.com>
2022-04-22 00:41:18 +0200

    ---

---
+++ exercise.js
@@ -0,0 +1,51 @@
+//scrivere un ciclo for che scriva a console i numeri da 10 a 50
+
+//1)
+// for (let i = 10 ; i <= 50; i++){
+//     console.log(i)
+// }
+
+
+
+//scrivere un ciclo for che scriva a console i numeri pari da 8 a 128
+
+// for( let i = 8; i <= 128 ; i++){
+//  const isPariNumeri = i % 2 === 0;
+
+//  let result="";
+
+// if(isPariNumeri){
+//     console.log(i + result)
+// }
+// }
+ 
+
+//scrivere un ciclo for che scriva a console i numeri da 100 a 0
+
+// for ( let i=100; i >= 0 ; i--){
+//     console.log(i)
+// }
+ 
+
+// let quaderno = 0
+// for ( let i=0; i <= 100; i++){
+
+//     quaderno = quaderno + i
+//     console.log(quaderno)
+//  }
+     
+// somma di 30 a ogni giro
+//  for ( let i=0; i <= 100; i++){
+
+//     let somma  =  i + 30
+//     console.log(somma)
+//  }
+ 
+//while 1
+
+// let counter =10;
+// while (counter <= 50) {
+//     console.log("numeri da 10 a: " + counter);
+//     counter = counter + 1;
+// }
+
